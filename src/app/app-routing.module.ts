f905ab2977578e57817f95ce2e2e200cd2f88bcf
quan <trandinhquan0711@gmail.com>
2022-08-31 17:23:14 +0700

    upProfile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProfileComponent} from "./profile/profile/profile.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'profile',component: ProfileComponent},
+  {path:'',component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
