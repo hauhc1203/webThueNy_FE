8501601e2dcf5213d84c2ce049e51f759ff334c1
duong2512 <tungduong25122002@gmail.com>
2022-09-06 22:47:49 +0700

    up validate login + register

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -10,6 +10,7 @@ import {Router} from "@angular/router";
 export class RegisterComponent implements OnInit {
   checkDuplicateMail:boolean = true;
   checkDuplicateUsername:boolean = true;
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
   constructor(private registerService:LoginService,private router:Router) { }
 
@@ -17,10 +18,10 @@ export class RegisterComponent implements OnInit {
   }
 
   registerForm= new FormGroup({
-    userName : new FormControl("",[Validators.required,Validators.minLength(10)]),
-    passWord : new FormControl("",Validators.required),
-    email : new FormControl ("",Validators.required),
-    phoneNumber : new FormControl ("",Validators.required)
+    userName : new FormControl("",[Validators.required,Validators.minLength(5)]),
+    passWord : new FormControl("",[Validators.required,Validators.minLength(5)]),
+    email : new FormControl("", [Validators.required,Validators.pattern("^((([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\\.([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\\d|-|\\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\\d|-|\\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$")]),
+    phoneNumber : new FormControl ("",[Validators.required,Validators.minLength(10),Validators.maxLength(10)])
   })
 
   // checkMail(){
@@ -50,6 +51,7 @@ export class RegisterComponent implements OnInit {
   // }
   register(){
     this.registerService.register(this.registerForm.value).subscribe((data)=>{
+      console.log(data)
       this.checkDuplicateUsername=data[0];
       this.checkDuplicateMail=data[1];
       if (data[0]&&data[1]){
